bb08b706f4112ae6782594555c378d6920c5c3f4
Andrew Castano <stainsteelcrown@gmail.com>
2014-09-11 20:43:45 -0600

    I got the screen to clear. The most basic idea.

--- application.py
+++ application.py
@@ -1,9 +1,11 @@
+import os
+    
 a = ['This is the beginning of the story']
 
 for i in range(0, 3):
     print a[i]
     nextLine = raw_input()
     a.append(nextLine)
+    os.system('clear')
 
-print "\n"
 print "\n".join(a)
