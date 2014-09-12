a8cd3bce88a1926abf256b869e431f408497980e
Andrew Castano <stainsteelcrown@gmail.com>
2014-09-11 20:52:06 -0600

    Adding Flask to the mix, testing it out.

--- application.py
+++ application.py
@@ -1,11 +1,18 @@
 import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def story_generation():    
+    a = ['This is the beginning of the story']
+
+    for i in range(0, 3):
+        return a[i]
+        nextLine = raw_input()
+        a.append(nextLine)
+        os.system('clear')
+
+    return "\n".join(a)
     
-a = ['This is the beginning of the story']
-
-for i in range(0, 3):
-    print a[i]
-    nextLine = raw_input()
-    a.append(nextLine)
-    os.system('clear')
-
-print "\n".join(a)
+if __name__ == '__main__':
+    app.run()
